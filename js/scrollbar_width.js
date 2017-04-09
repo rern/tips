0540ea530c2cdc7d397f6263c8ad1b782fe39206
rern <rernrern@gmail.com>
2017-04-09 16:23:33 +0700

    Create scrollbar_width.js

---
+++ js/scrollbar_width.js
@@ -0,0 +1,9 @@
+// get scrollbar width
+var scrollDiv = document.createElement("div");
+var scrollCss = 'width: 100px; height: 100px; overflow: scroll; position: absolute; top: -9999px;'
+scrollDiv.style.cssText = scrollCss;
+document.body.appendChild(scrollDiv);
+
+var scrollWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+
+document.body.removeChild(scrollDiv);
