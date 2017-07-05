cb2d8075529f0258198a90b676a81dfb5c781da9
rern <rernrern@gmail.com>
2017-07-05 08:51:43 +0700

    Create f_password.sh

---
+++ bash/f_password.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# password promt funtion
+# set password variable as '$pwd1'
+
+info=$( echo $(tput setab 6; tput setaf 0) i $(tput setab 0; tput setaf 7) )
+
+setpwd() {
+	echo
+	echo 'Password: '
+	read -s pwd1
+	echo
+	echo 'Retype password: '
+	read -s pwd2
+	echo
+	if [[ $pwd1 != $pwd2 ]]; then
+		echo
+		echo "$info Passwords not matched. Try again."
+		setpwd
+	fi
+}
