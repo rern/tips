813206b1cc9a8367be5030c0dc9325252703cb63
rern <rernrern@gmail.com>
2017-07-10 19:06:28 +0700

    Update f_heading.sh

--- bash/f_heading.sh
+++ bash/f_heading.sh
@@ -1,109 +1,98 @@
 #!/bin/bash
 
-# heading variables and functions
-
-# linecolor <character> [color]
-
-# textcolor '<string>' [color] [background]
-
-# title2 "<string>"
-# =============================================>> (fill width)
-# [   ] string
-# =============================================>> (fill width)
-
-# titlebar "<string>"
-# -------------------------------------------->> (fill width)
-# [   ] string
-# -------------------------------------------->> (fill width)
-
-# titleinfo "<string>"
-# -------------------------------------------->> (fill width)
-# [ i ] string
-# -------------------------------------------->> (fill width)
-
-# title "<string>"
-# -------------------------------------------->> (fill width)
-# string
-# -------------------------------------------->> (fill width)
-
-# titleend "<string>"
-# string
-# -------------------------------------------->> (fill width)
-
-# error "<string>"
-# (red) -------------------------------------->> (fill width)
-# [ ! ] string
-# (red) -------------------------------------->> (fill width)
-
-# errorend "<string>"
-# string
-# (red) -------------------------------------->> (fill width)
-
-# 0 - black
-# 1 - red
-# 2 - green
-# 3 - yellow
-# 4 - blue
-# 5 - magenta
-# 6 - cyan
-# 7 - gray (default)
+# Draw colored string with top-bottom lines
 
 linecolor() {
 	if [[ -z $2 ]] || (( $2 > 6 )); then
-		color='\e[0;36m%*s\e[m\n'
+		local color='\e[0;36m%*s\e[m\n'
 	else
-		color='\e[0;3'$2'm%*s\e[m\n'
+		local color='\e[0;3'$2'm%*s\e[m\n'
 	fi
 	printf $color "${COLUMNS:-$(tput cols)}" '' | tr ' ' "$1"
 }
 textcolor() { 
-	[[ $2 ]] && color=$2 || color=6
-	[[ $3 ]] && back=$3 || back=0
-	echo $(tput setaf "$color"; tput setab "$back")"$1"$(tput setaf 7; tput setab 0)
+	local color=6   # default
+	local back=0  # default
+	[[ $2 ]] && local color=$2
+	[[ $3 ]] && local back=$3
+	echo "$(tput setaf $color; tput setab $back)${1}$(tput setaf 7; tput setab 0)"
 }
 
-line2=$( linecolor = )         # =         (cyan - default)
-line=$( linecolor - )          # -         (cyan - default)
-linered=$( linecolor - 1 )     # -         (red)
-bar=$( textcolor '   ' 7 6 )   # [   ]     (gray on cyan)    - double quoted to keep spaces
-info=$( textcolor ' i ' 0 3 )  # [ i ]     (black on yellow) - double quoted to keep spaces
-warn=$( textcolor ' ! ' 7 1 )  # [ ! ]     (gray on red)     - double quoted to keep spaces
+bar=$( textcolor ' . ' 6 6 )   # [   ]     (gray on cyan)    - double quoted to keep spaces
+info=$( textcolor ' i ' 0 3 )  # [ i ]     (black on yellow)
+warn=$( textcolor ' ! ' 7 1 )  # [ ! ]     (gray on red)
+
+title() {
+	
+	local color=6
+	local line='-'
+	local notop=0
+	local nobottom=0
+	
+	usage() {
+		linecolor -
+		echo "Usage:  $0 [OPTION]... STRING"
+		linecolor -
+		echo 'STRING: $( textcolor STRING [color] [background] )'
+		echo
+		echo 'Draw colored' $( textcolor STRING ) 'with top-bottom lines'
+		echo 'OPTION:'
+		echo '    -c N   N - line color'
+		echo '    -l C   C - line character'
+		echo '    -nt    no top line'
+		echo '    -nb    no bottom line'
+		echo 'Badge:'
+		echo '    '$bar'    $bar'   
+		echo '    '$info'    $info'   
+		echo '    '$warn'    $warn'   
+		echo 'Color code N:'
+		echo '    '$( textcolor '0 -' 7 ) 'black'
+		echo '    '$( textcolor '1 -' 1 ) 'red'
+		echo '    '$( textcolor '2 -' 2 ) 'green'
+		echo '    '$( textcolor '3 -' 3 ) 'yellow'
+		echo '    '$( textcolor '4 -' 4 ) 'blue'
+		echo '    '$( textcolor '5 -' 5 ) 'magenta'
+		echo '    '$( textcolor '6 -' 6 ) 'cyan (default)1'
+		echo '    '$( textcolor '7 -' 7 ) 'gray'
+	}
+	while :; do
+		case $1 in
+			-h|-\?|--help) usage; exit 1;;
+			-c) local color=$2; shift;;
+			-l) local line=$2; shift;;
+			-nt) local notop=1;; # no 'shift' for non-argument option
+			-nb) local nobottom=1;;
+			-?*) printf 'WARN: unknown option: %s\n' "$1" >&2;;
+			*) break
+		esac
+		shift
+	done
+
+	[[ $notop == 0 ]] && echo $( linecolor $line $color )
+	echo -e "${@}" # $@ > "${@}" - preserve spaces 
+	[[ $nobottom == 0 ]] && echo $( linecolor $line $color )
+}
 
 title2() {
-	echo $line2
-	echo -e "$bar $1"
-	echo $line2
-	echo
+	title -l = $bar $@
 }
 titlebar() {
-	echo $line
-	echo -e "$bar $1"
-	echo $line
+	title $bar $@
 	echo
 }
 titleinfo() {
-	echo $line
-	echo -e "$info $1"
-	echo $line
+	title $info $@
 	echo
 }
-title() {
-	echo $line
-	echo -e "$1"
-	echo $line
-}
 titleend() {
-	echo -e "\n$1"
-	echo $line
+	title -nt $@
 	echo
 }
 error() {
-	echo $linered
-	echo $warn $1
-	echo $linered
+	title -c 1 $warn $@
+	echo
 }
 errorend() {
-	echo -e "\n$warn $1"
-	echo $linered
+	title -nt -c 1 $warn $@
 	echo
 }
